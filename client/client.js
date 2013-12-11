af524f643aeba910afd0eef9733fb4d6838e76fb
pheras <pheras@gmail.com>
2013-12-11 23:43:12 +0100

    Layout changes of SimpleWebRTC video

--- client/client.js
+++ client/client.js
@@ -43,6 +43,24 @@ Meteor.startup(function(){
     $(document).on("click", ".alert .close", function(e) {
 	$(this).parent().hide();
     });
+
+
+    ///////////////
+    // SimpleWebRTC
+    var webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: 'remotesVideos',
+      // immediately ask for camera access
+      autoRequestMedia: true
+    });
+
+    // we have to wait until it's ready
+    webrtc.on('readyToCall', function () {
+      // you can name it anything
+      webrtc.joinRoom('Plataphorma');
+    });
 });
 
 
