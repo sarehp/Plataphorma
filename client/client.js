826ff121e79ad8bd5f6ccb0bb89ae78b6234159f
pheras <pheras@gmail.com>
2014-11-11 10:30:37 +0100

    Repaired minor problems

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
- *   Subscriptions to collections published by the server
- */
+*   Subscriptions to collections published by the server
+*/
 
 // Meteor.users is a collection automatically created in the server by
 // user_password package. This subscription gives the client access to
@@ -12,12 +12,12 @@ Meteor.subscribe("all_games");
 
 
 /*
- *  Session.get() is a reactive data source. Thanks to Deps.autorun,
- *  the function passed as a parameter is rerun automatically
- *  everytime the session variable current_game changes (i.e. when
- *  user clicks on a new game button).
- */
-Deps.autorun(function(){
+*  Session.get() is a reactive data source. Thanks to Tracker.autorun,
+*  the function passed as a parameter is rerun automatically
+*  everytime the session variable current_game changes (i.e. when
+*  user clicks on a new game button).
+*/
+Tracker.autorun(function(){
     var current_game = Session.get("current_game");
 
     // The server will publish a different Messages collection to the
@@ -33,46 +33,28 @@ Deps.autorun(function(){
 
 
 /*******************************************************************************
- *   Startup code on the client
- */
+*   Startup code on the client
+*/
 Meteor.startup(function(){
     Session.set("current_game", "none");
     $('#gamecontainer').hide();
     $('#container').hide();
 
     $(document).on("click", ".alert .close", function(e) {
-	$(this).parent().hide();
-    });
-
-
-    ///////////////
-    // SimpleWebRTC
-    var webrtc = new SimpleWebRTC({
-      // the id/element dom element that will hold "our" video
-      localVideoEl: 'localVideo',
-      // the id/element dom element that will hold remote videos
-      remoteVideosEl: 'remotesVideos',
-      // immediately ask for camera access
-      autoRequestMedia: true
-    });
-
-    // we have to wait until it's ready
-    webrtc.on('readyToCall', function () {
-      // you can name it anything
-      webrtc.joinRoom('Plataphorma');
+        $(this).parent().hide();
     });
 });
 
 
 
 /*******************************************************************************
- *  For illustration purpose: Meteor.userId() is a reactive data source.  
- *  Thanks to Deps.autorun, the function passed as a parameter will be rerun
- *  automatically every time the user id changes (i.e. when user signs
- *  in and signs off
- */
+*  For illustration purpose: Meteor.userId() is a reactive data source.
+*  Thanks to Tracker.autorun, the function passed as a parameter will be rerun
+*  automatically every time the user id changes (i.e. when user signs
+*  in and signs off
+*/
 var currentUser = null;
-Deps.autorun(function(){
+Tracker.autorun(function(){
     console.log("current user: " + currentUser);
     currentUser = Meteor.userId();
     console.log("current user: " + currentUser);
@@ -82,8 +64,8 @@ Deps.autorun(function(){
 
 
 /*******************************************************************************
- *  Template helper functions
- */
+*  Template helper functions
+*/
 
 
 Template.messages.messages = function () {
@@ -92,8 +74,8 @@ Template.messages.messages = function () {
     var messages = [];
 
     messagesColl.forEach(function(m){
-	var userName = Meteor.users.findOne(m.user_id).username;
-	messages.push({name: userName , message: m.message});
+        var userName = Meteor.users.findOne(m.user_id).username;
+        messages.push({name: userName , message: m.message});
     });
 
     return messages;
@@ -101,27 +83,27 @@ Template.messages.messages = function () {
 
 Template.input.events = {
     'keydown input#message' : function (event) {
-	if (event.which == 13) { 
-	    if (Meteor.userId()){
-		var user_id = Meteor.user()._id;	    
-		var message = $('#message');
-		if (message.value != '') {
-		    Messages.insert({
-			user_id: user_id,
-			message: message.val(),
-			time: Date.now(),
-			game_id: Session.get("current_game")
-		    });
-		    message.val('')
-		}
-	    }
-	    else {
-		$("#login-error").show();
-	    }
-	}
-    }
+        if (event.which == 13) {
+            if (Meteor.userId()){
+                var user_id = Meteor.user()._id;
+                var message = $('#message');
+                if (message.value != '') {
+                    Messages.insert({
+                        user_id: user_id,
+                        message: message.val(),
+                        time: Date.now(),
+                        game_id: Session.get("current_game")
+                    });
+                    message.val('')
+                }
+            }
+            else {
+                $("#login-error").show();
+            }
+        }
+    } 
 }
-    
+
 
 Template.choose_game.games = function (){
     return Games.find();
@@ -129,21 +111,21 @@ Template.choose_game.games = function (){
 
 Template.choose_game.events = {
     'click #AlienInvasion': function () {
-	$('#gamecontainer').hide();
-	$('#container').show();
-	var game = Games.findOne({name:"AlienInvasion"});
-	Session.set("current_game", game._id);
+        $('#gamecontainer').hide();
+        $('#container').show();
+        var game = Games.findOne({name:"AlienInvasion"});
+        Session.set("current_game", game._id);
     },
     'click #FrootWars': function () {
-	$('#container').hide();
-	$('#gamecontainer').show();
-	var game = Games.findOne({name:"FrootWars"});
-	Session.set("current_game", game._id);
+        $('#container').hide();
+        $('#gamecontainer').show();
+        var game = Games.findOne({name:"FrootWars"});
+        Session.set("current_game", game._id);
     },
     'click #none': function () {
-	$('#container').hide();
-	$('#gamecontainer').hide();
-	Session.set("current_game", "none");
+        $('#container').hide();
+        $('#gamecontainer').hide();
+        Session.set("current_game", "none");
     }
 }
 
@@ -155,7 +137,7 @@ Template.best_players.none = function (){
 Template.best_players.gameName = function (){
     var game_id = Session.get("current_game");
     if (game_id)
-	var game_name = Games.findOne({_id: game_id}).name;
+        var game_name = Games.findOne({_id: game_id}).name;
     return game_name;
 }
 
@@ -167,35 +149,32 @@ Template.chat.none = function (){
 Template.chat.gameName = function (){
     var game_id = Session.get("current_game");
     if (game_id)
-	var game_name = Games.findOne({_id: game_id}).name;
+        var game_name = Games.findOne({_id: game_id}).name;
     return game_name;
 }
 
 
 Template.best_players.best_players = function (){
     var matches =  Matches.find({}, {limit:5, sort: {points:-1}});
-    
+
     var users_data = [];
-    
+
     matches.forEach (function (m) {
-   	var user = Meteor.users.findOne({_id: m.user_id});
-  	if (user){
-	    var game = Games.findOne({_id: m.game_id});
-   	    users_data.push({name: user.username, game: game.name, points: m.points});
-  	}
+        var user = Meteor.users.findOne({_id: m.user_id});
+        if (user){
+            var game = Games.findOne({_id: m.game_id});
+            users_data.push({name: user.username, game: game.name, points: m.points});
+        }
     });
-    
+
     return users_data;
 }
 
 
 
 /*******************************************************************************
-*  Configuration of signup 
+*  Configuration of signup
 */
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
-
-
-
